6aae6258561b462e52e379a766e60b7b2ca2d489
victor <victorr.dantas@gmail.com>
2024-10-23 17:56:21 -0400

    Fix miscellaneous operators

--- 11_miscellaneous_operators.pl
+++ 11_miscellaneous_operators.pl
@@ -13,10 +13,21 @@ use warnings;
 my $word1 = "Hello";
 
 # String concatenation
-$word1 .= ", World";
+$word1 .= "World";
 
 print "$word1\n";
 
 # String Multiplication (I WILL SEARCH)
 
+my $repetead_word1 = $word1 x 3;
+print "$repetead_word1\n";
+
 # Range Operator (I WILL SEARCH)
+
+my @array_num = (1..10);
+
+foreach(@array_num) {
+    print "$_ ";
+}
+
+print "\n";
