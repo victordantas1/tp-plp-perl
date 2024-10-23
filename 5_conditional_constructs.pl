0d4e160ba21008771e830af30192d9a2b7da713b
victor <victorr.dantas@gmail.com>
2024-10-23 16:10:38 -0400

    Add more loop constructs

--- 5_conditional_constructs.pl
+++ 5_conditional_constructs.pl
@@ -4,21 +4,24 @@ use warnings;
 my $animal = "Toby"; # String
 my $animal_age = 2; # Integer
 my $animal_weight = 1.5; # Float
+my @animals = ("dog", "cat", "mouse");
 
 # Conditional Constructs
 
 print "Verify weight animal: $animal, ";
 if ($animal_weight < 2) {
-    print "under weight animal!\n"
+    print "under weight animal!\n";
 }
 elsif ($animal_weight <= 4){
     print "normal weight animal!\n";
 }
 else {
-    print "overweight animal!\n"
+    print "overweight animal!\n";
 }
 
 # unless is equivalent to if ( !condition )
 unless (@animals > 5) {
     print("We have less then 5 animals\n");
 }
+
+print "Animal age: $animal_age\n" if $animal_age >= 2;
--- 6_loop_constructs.pl
+++ 6_loop_constructs.pl
@@ -3,20 +3,59 @@ use warnings;
 
 my @animals = ("dog", "cat", "mouse");
 my @animals_ages = (4, 2, 1);
-my @mixed = ("dog", 4, "cat", 2, "mouse", 1);
+my @cars = ("Corsa", "Celta", "Kwid", "Palio");
+my %fruit_colors = (
+    apple => "red",
+    banana => "yellow"
+);
 
 # Looping Constructs
 
+# For Loop
+print "For Loop: \n";
 print "Array of animals: ";
 for (my $i = 0; $i < @animals; $i++) {
     print $animals[$i], " ";
 }
-print "\n";
+print "\n\n";
 
+# While Loop
+print "While Loop: \n";
 print "Array of animal ages: ";
 my $i = 0;
 while ($i < 3) {
     print $animals_ages[$i], " ";
     $i++;
 }
+
+# We can use while in post-condition
+# print "LALALALALA\n" while 1;
+
+print "\n\n";
+
+# Until Loop (same while(!condition) )
+print "Until Loop: \n";
+print "Array of animal: ";
+until($i == 0) {
+    $i--;
+    print "$animals[$i] ";
+}
+
+print "\n\n";
+
+# Foreach Loop
+print "Foreach Loop: \n";
+print "Array of Cars: ";
+foreach(@cars) {
+    print "$_ ";
+}
 print "\n";
+
+print "Array of animals: ";
+print "$animals[$_] " foreach 0 ... @animals - 1;
+print "\n";
+
+print "Hash of Fruits and your Colors:\n";
+for my $key (keys %fruit_colors) {
+    print("Value of $key is $fruit_colors{$key}\n");
+}
